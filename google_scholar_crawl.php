298816ea3458e784d68fd2c1c68a6211fca6045c
alif <alifianfajarpratama@gmail.com>
2022-06-09 23:18:53 +0700

    Crawl done

--- google_scholar_crawl.php
+++ google_scholar_crawl.php
@@ -1,11 +1,14 @@
 <?php
 require_once __DIR__ . '/vendor/autoload.php';
 include_once('simple_html_dom.php');
-
+error_reporting(E_ERROR | E_PARSE);
 $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "iir_uas";
+
+set_time_limit(300);
+$jum_index = 0;
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -31,7 +34,8 @@ $html = file_get_html($url);
 $news = [];
 
 $crawl = (isset($_GET['is_crawl'])) ? true : false;
-
+// var_dump($crawl);
+// die;
 if ($crawl) {
     foreach ($html->find('div[class="gs_ri"]') as $index => $berita) {
         $title = strip_tags($berita->find('a', 0)->innertext);
@@ -60,65 +64,114 @@ if ($res->num_rows > 0) {
 }
 
 foreach ($journals as $index => $j) {
+    $jum_index += $index;
     $link = $j['link'];
     $cut = explode("/", $link);
-    if (str_contains($link, 'pdf')) {
-        $abstract = "Tidak bisa crawl file pdf";
-    } else {
-        if ($cut[2] == "link.springer.com") {
-            $opts = array(
-                'http' => array(
-                    'method' => "GET",
-                    'header' => "User-Agent: lashaparesha api script\r\n"
-                )
-            );
-
-            $context = stream_context_create($opts);
-
-            // $url = http://www.giantbomb.com/api/..........
-            // $link2 = "https://link.springer.com/article/10.1057/jors.1992.4";
-            $html2 = file_get_html($link, false, $context);
-            // echo $file;
-            // // echo file_get_html("www.google.com", false, $context);
-            // $html2 = file_get_html($link, false, $context);
-            foreach ($html2->find('div[id="Abs1-content"]') as $index2 => $berita2) {
-                $abstract = $berita2->find('p', 0)->innertext;
-
-                // echo $abstract;
-            }
-        } else if ($cut[2] == "dl.acm.org") {
-
-            $opts = array(
-                'http' => array(
-                    'method' => "GET",
-                    'header' => "User-Agent: lashaparesha api script\r\n"
-                )
-            );
-
-            $context = stream_context_create($opts);
-
-            // $url = http://www.giantbomb.com/api/..........
-            // $link2 = "https://link.springer.com/article/10.1057/jors.1992.4";
-            $html2 = file_get_html($link, false, $context);
-            // echo $file;
-            // // echo file_get_html("www.google.com", false, $context);
-            // $html2 = file_get_html($link, false, $context);
-            foreach ($html2->find('div[class="abstractSection abstractInFull"]') as $index2 => $berita2) {
-                $abstract = $berita2->find('p', 0)->innertext;
-
-                // echo $abstract;
-            }
+    $data = [];
+    try {
+        if (str_contains($link, 'pdf')) {
+            $abstract = "Tidak bisa crawl file pdf";
         } else {
-            $abstract = "";
+
+            $opts = array(
+                'http' => array(
+                    'method' => "GET",
+                    'header' => "Accept-language: en\r\n" .
+                        "User-Agent:    Mozilla/5.0 (Windows; U; Windows NT 6.0; en-US; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6\r\n" .
+                        "Cookie: foo=bar\r\n"
+                )
+            );
+            $context = stream_context_create($opts);
+
+            $html = file_get_html($link, false, $context);
+            $found = false;
+            foreach ($html->find('p') as  $p) {
+                $word_count = str_word_count($p);
+                if ($word_count > 200 && $word_count <= 300) {
+                    $found = true;
+                    // var_dump($j);
+                    // echo "<br>";
+                    // echo ($p);
+                    // die;
+                    $data[] = [
+                        "id" => $j['id'],
+                        "title" => $j["title"],
+                        "abstract" => $p->plaintext,
+                        "authors" => "authors_test",
+                        "cite" => $j['cite']
+                    ];
+                }
+            }
+
+            // foreach ($html->find('span') as $p) {
+            //     $word_count = str_word_count($p);
+            //     if ($word_count > 200 && $word_count <= 300) {
+
+            //         $found = true;
+            //         $data[] = [
+            //             "id" => $j['id'],
+            //             "title" => $j["title"],
+            //             "abstract" => $p,
+            //             "authors" => "authors_test",
+            //             "cite" => $j['cite']
+            //         ];
+            //     }
+            // }
+
+            // foreach ($html->find('article') as $p) {
+            //     $word_count = str_word_count($p);
+            //     if ($word_count > 200 && $word_count <= 300) {
+
+            //         $found = true;
+            //         $data[] = [
+            //             "id" => $j['id'],
+            //             "title" => $j["title"],
+            //             "abstract" => $p,
+            //             "authors" => "authors_test",
+            //             "cite" => $j['cite']
+            //         ];
+            //     }
+            // }
         }
+    } catch (\Throwable $th) {
+    }
+
+
+    if ($found) {
+
+        insertJournalData($j["id"], $data, $conn);
     }
 
     // SETELAH CRAWL ABSTRACT DAN AUTHOR
     // MASUKKAN KE TABLE JOURNALS
 
     // INSERT ......
+
 }
 
+
+
+$sql = 'SELECT * FROM journal_data';
+$res = $conn->query($sql);
+
+
+$data = [];
+
+if ($res->num_rows > 0) {
+    $data = $res->fetch_all(MYSQLI_ASSOC);
+}
+
+var_dump($jum_index);
+
+
+
+function insertJournalData($id, $data, $conn)
+{
+    $stmt = $conn->prepare("INSERT INTO journal_data (title, abstract, authors, cite, crawl_id) VALUES (?, ?, ?, ?, ?)");
+    $stmt->bind_param("sssis", $data[0]["title"], $data[0]["abstract"], $data[0]["authors"], $data[0]["cite"], $id);
+    $stmt->execute();
+    // var_dump($conn->error_list);
+}
 
 
 
--- test.php
+++ test.php
@@ -1,24 +1,39 @@
 <?php
 include_once('simple_html_dom.php');
+error_reporting(E_ERROR | E_PARSE);
+echo "start";
+echo "<br>";
+try {
 
-$opts = array(
-    'http' => array(
-        'method' => "GET",
-        'header' => "Accept-language: en\r\n" .
-            "User-Agent:    Mozilla/5.0 (Windows; U; Windows NT 6.0; en-US; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6\r\n" .
-            "Cookie: foo=bar\r\n"
-    )
-);
-$context = stream_context_create($opts);
-$url = "https://dl.acm.org/doi/abs/10.1145/3332184";
+    $opts = array(
+        'http' => array(
+            'method' => "GET",
+            'header' => "Accept-language: en\r\n" .
+                "User-Agent:    Mozilla/5.0 (Windows; U; Windows NT 6.0; en-US; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6\r\n" .
+                "Cookie: foo=bar\r\n"
+        )
+    );
+    $context = stream_context_create($opts);
+    $url = "https://www.nature.com/articles/s41579-018-0118-9.";
 
-$html = file_get_html($url, false, $context);
+    $html = file_get_html($url, false, $context);
 
-foreach ($html->find('p') as $index => $j) {
-    # code...
-    echo $j  . ' count :  ' . strlen($j) . '<br>';
+    foreach ($html->find('p') as $index => $j) {
+        # code...
+        $c = str_word_count($j);
+        if ($c > 200 && $c <= 300) {
+            var_dump($j->plaintext);
+        }
+        // echo $j  . ' count :  ' . strlen($j) . '<br>';
+        // echo "<br>";
+        // echo str_word_count($j);
+        // COUNT 1407
+        // WORD COUNT 202
+    }
+} catch (Throwable $th) {
+
+    echo "TEST THROW";
     echo "<br>";
-    echo str_word_count($j);
-    // COUNT 1407
-    // WORD COUNT 202
 }
+echo "<br>";
+echo "end";
